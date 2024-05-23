c3f2979ff3e57ce66e516da52b586a107b97c1f5
ShaiShillo <shaishillo@gmail.com>
2024-05-24 01:05:19 +0300

    Updated Hello.java with new print message (Fifth try)

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         for (int i=1;i<=10; i++) {
-            System.out.println("Updated (Forth Time) Hello World ..."+i);
+            System.out.println("Updated (Fifth Time) Hello World ..."+i);
         }
     }
 }
